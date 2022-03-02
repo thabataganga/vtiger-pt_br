f6e79c2b30d5ca787c5c3a0da4b918f530dff5da
thabataganga <ganga.thabata@gmail.com>
2022-03-02 18:15:49 -0300

    Accounts

---
+++ pt_br/Accounts.php
@@ -0,0 +1,64 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+	// Basic Strings
+	'Accounts' => 'Organizações',
+	'SINGLE_Accounts' => 'Organização',
+	'LBL_ADD_RECORD' => 'Adicionar organização',
+	'LBL_RECORDS_LIST' => 'Lista de organizações',
+
+	// Blocks
+	'LBL_ACCOUNT_INFORMATION' => 'Detalhes da organização',
+
+	// Mass Action
+	'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Hierarquia da organização',
+
+	//Field Labels
+	'industry' => 'Indústria',
+	'Account Name' => 'Nome da organização',
+	'Account No' => 'Número da organização',
+	'Website' => 'Website',
+	'Ticker Symbol' => 'Símbolo ticker',
+	'Member Of' => 'Membro de',
+	'Employees' => 'Funcionários',
+	'Ownership' => 'Proprietário',
+	'SIC Code' => 'Código SIC',
+	'Other Email' => 'Email secundário',
+	'Other Phone' => 'Telefone secundário',
+	'Phone' => 'Telefone principal',
+	'Email' => 'E-mail primário',
+	'Type' => 'Tipo',
+
+	//Added for existing picklist entries
+	'Analyst'=>'Analista',
+	'Competitor'=>'Concorrente',
+	'Customer'=>'Cliente',
+	'Integrator'=>'Integrador',
+	'Investor'=>'Investidor',
+	'Press'=>'Pressionar',
+	'Prospect'=>'Prospect',
+	'Reseller'=>'Revendedor',
+	'LBL_START_DATE' => 'Data de início',
+	'LBL_END_DATE' => 'Data final',
+
+	//Duplication error message
+	'LBL_DUPLICATES_EXIST' => 'O nome da organização já existe',
+	'LBL_COPY_SHIPPING_ADDRESS' => 'Copiar endereço de entrega',
+	'LBL_COPY_BILLING_ADDRESS' => 'Copiar endereço de cobrança',
+	'LBL_IMAGE_INFORMATION' => 'Foto do perfil',
+	'Organization Image' => 'Imagem da organização',
+);
+
+$jsLanguageStrings = array(
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Tem certeza de que deseja excluir?',
+	'LBL_DELETE_CONFIRMATION' => 'A exclusão desta Organização removerá suas Oportunidades e Cotações relacionadas. Tem certeza de que deseja excluir esta organização?',
+	'LBL_MASS_DELETE_CONFIRMATION' => 'A exclusão desta(s) Organização(ões) removerá suas Oportunidades e Cotações relacionadas. Tem certeza de que deseja excluir os registros selecionados?',
+	'JS_DUPLICATE_CREATION_CONFIRMATION' => 'O nome da organização já existe. Deseja criar um registro duplicado?'
+);
