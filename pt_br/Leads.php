5f18944242c9db9bdfdacef16350f551055c95dd
thabataganga <ganga.thabata@gmail.com>
2022-03-02 21:24:26 -0300

    Leads

--- pt_br/Leads.php
+++ pt_br/Leads.php
@@ -11,62 +11,62 @@ $languageStrings = array(
 	// Basic Strings
 	'Leads' => 'Leads',
 	'SINGLE_Leads' => 'Lead',
-	'LBL_RECORDS_LIST' => 'Leads List',
-	'LBL_ADD_RECORD' => 'Add Lead',
+	'LBL_RECORDS_LIST' => 'Listar Leads',
+	'LBL_ADD_RECORD' => 'Adicionar  Lead',
 
 	// Blocks
-	'LBL_LEAD_INFORMATION' => 'Lead Details',
+	'LBL_LEAD_INFORMATION' => 'Detalhes da Lead',
 
 	//Field Labels
-	'Lead No' => 'Lead Number',
-	'Company' => 'Company',
-	'Designation' => 'Designation',
+	'Lead No' => 'Número da Lead',
+	'Company' => 'Empresa',
+	'Designation' => 'Designação',
 	'Website' => 'Website',
-	'Industry' => 'Industry',
-	'Lead Status' => 'Lead Status',
-	'No Of Employees' => 'Number of Employees',
-	'Phone' => 'Primary Phone',
-	'Secondary Email' => 'Secondary Email',
-	'Email' => 'Primary Email',
+	'Industry' => 'Indústria',
+	'Lead Status' => 'Status da Lead',
+	'No Of Employees' => 'Número de funcionários',
+	'Phone' => 'Telefone principal',
+	'Secondary Email' => 'E-mail Secundário',
+	'Email' => 'E-mail principal',
 
 	//Added for Existing Picklist Entries
 
-	'--None--'=>'--None--',
-	'Mr.'=>'Mr.',
-	'Ms.'=>'Ms.',
-	'Mrs.'=>'Mrs.',
-	'Dr.'=>'Dr.',
-	'Prof.'=>'Prof.',
+	'--None--' => '--Nenhum--',
+	'Mr.' => 'Sr.',
+	'Ms.' => 'Sra.',
+	'Mrs.' => 'Srta.',
+	'Dr.' => 'Dr.',
+	'Prof.' => 'Prof.',
 
 	//Lead Status Picklist values
-	'Attempted to Contact'=>'Attempted to Contact',
-	'Cold'=>'Cold',
-	'Contact in Future'=>'Contact in Future',
-	'Contacted'=>'Contacted',
-	'Hot'=>'Hot',
-	'Junk Lead'=>'Junk Lead',
-	'Lost Lead'=>'Lost Lead',
-	'Not Contacted'=>'Not Contacted',
-	'Pre Qualified'=>'Pre Qualified',
-	'Qualified'=>'Qualified',
-	'Warm'=>'Warm',
+	'Attempted to Contact' => 'Tentativa de contato',
+	'Cold' => 'Frio',
+	'Contact in Future' => 'Contato no Futuro',
+	'Contacted' => 'Contactado',
+	'Hot' => 'Quente',
+	'Junk Lead' => 'Junk Lead',
+	'Lost Lead' => 'Lead Perdida',
+	'Not Contacted' => 'Não contatado',
+	'Pre Qualified' => 'Pré Qualificado',
+	'Qualified' => 'Qualificado',
+	'Warm' => 'Morno',
 
 	// Mass Action
-	'LBL_CONVERT_LEAD' => 'Convert Lead',
+	'LBL_CONVERT_LEAD' => 'Lead convertida',
 
 	//Convert Lead
-	'LBL_TRANSFER_RELATED_RECORD' => 'Transfer related record to',
-	'LBL_CONVERT_LEAD_ERROR' => 'You have to enable either Organization or Contact to convert the Lead',
-	'LBL_LEADS_FIELD_MAPPING_INCOMPLETE' => 'Leads Field Mapping is incomplete(Settings > Module Manager > Leads > Leads Field Mapping)',
-	'LBL_LEADS_FIELD_MAPPING' => 'Leads Field Mapping',
+	'LBL_TRANSFER_RELATED_RECORD' => 'Transferir registro relacionado para',
+	'LBL_CONVERT_LEAD_ERROR' => 'Você precisa habilitar Organização ou Contato para converter o Lead',
+	'LBL_LEADS_FIELD_MAPPING_INCOMPLETE' => 'Mapeamento de campo de leads está incompleto (Configurações > Gerenciador de módulo > Leads > Mapeamento de campo de leads)',
+	'LBL_LEADS_FIELD_MAPPING' => 'Mapeamento de Campo de Leads',
 
 	//Leads Custom Field Mapping
-	'LBL_CUSTOM_FIELD_MAPPING'=> 'Lead Conversion Data Mapping',
-	'LBL_IMAGE_INFORMATION' => 'Profile Picture',
-	'Lead Image' => 'Lead Image',
+	'LBL_CUSTOM_FIELD_MAPPING' => 'Mapeamento de dados de conversão de leads',
+	'LBL_IMAGE_INFORMATION' => 'Foto do perfil',
+	'Imagem principal' => 'Imagem principal',
 );
 $jsLanguageStrings = array(
-	'JS_SELECT_CONTACTS' => 'Select Contacts to proceed',
-	'JS_SELECT_ORGANIZATION' => 'Select Organization to proceed',
-	'JS_SELECT_ORGANIZATION_OR_CONTACT_TO_CONVERT_LEAD' => 'Conversion requires selection of Contact or Organization'
+	'JS_SELECT_CONTACTS' => 'Selecione os contatos para continuar',
+	'JS_SELECT_ORGANIZATION' => 'Selecione a organização para prosseguir',
+	'JS_SELECT_ORGANIZATION_OR_CONTACT_TO_CONVERT_LEAD' => 'A conversão requer a seleção de Contato ou Organização'
 );
