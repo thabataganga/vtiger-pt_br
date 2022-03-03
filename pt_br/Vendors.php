a48d04ff520def09ad2f89eda6737009cd83481c
thabataganga <ganga.thabata@gmail.com>
2022-03-02 23:12:23 -0300

    Vendors

--- pt_br/Vendors.php
+++ pt_br/Vendors.php
@@ -9,38 +9,38 @@
  *************************************************************************************/
 $languageStrings = array(
 	// Basic Strings
-	'Vendors' => 'Vendors',
-	'SINGLE_Vendors' => 'Vendor',
-	'LBL_ADD_RECORD' => 'Add Vendor',
-	'LBL_RECORDS_LIST' => 'Vendors List',
+	'Vendors' => 'Fornecedores',
+	'SINGLE_Vendors' => 'Fornecedor',
+	'LBL_ADD_RECORD' => 'Adicionar fornecedor',
+	'LBL_RECORDS_LIST' => 'Listar fornecedores',
 
 	// Blocks
-	'LBL_VENDOR_INFORMATION' => 'Vendor Details',
-	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Address Details',
+	'LBL_VENDOR_INFORMATION' => 'Detalhes do fornecedor',
+	'LBL_VENDOR_ADDRESS_INFORMATION' => 'Detalhes do endereço',
 
 	//Field Labels
-	'Vendor Name' => 'Vendor Name',
-	'Vendor No' => 'Vendor Number',
+	'Vendor Name' => 'Nome do Fornecedor',
+	'Vendor No' => 'Número do fornecedor',
 	'Website' => 'Website',
-	'GL Account' => 'GL Account',
-	'Phone' => 'Primary Phone',
-	'Email' => 'Primary Email',
+	'GL Account' => 'Conta GL',
+	'Phone' => 'Telefone principal',
+	'Email' => 'E-mail principal',
 
 	//Added for existing Picklist entries
 
-	'300-Sales-Software'=>'300-Sales-Software',
-	'301-Sales-Hardware'=>'301-Sales-Hardware',
-	'302-Rental-Income'=>'302-Rental-Income',
-	'303-Interest-Income'=>'303-Interest-Income',
-	'304-Sales-Software-Support'=>'304-Sales-Software-Support',
-	'305-Sales Other'=>'305-Sales Other',
-	'306-Internet Sales'=>'306-Internet Sales',
-	'307-Service-Hardware Labor'=>'307-Service-Hardware Labor',
-	'308-Sales-Books'=>'308-Sales-Books',
+	'300-Sales-Software' => '300-Sales-Software',
+	'301-Sales-Hardware' => '301-Sales-Hardware',
+	'302-Rental-Income' => '302-Rental-Income',
+	'303-Interest-Income' => '303-Interest-Income',
+	'304-Sales-Software-Support' => '304-Sales-Software-Support',
+	'305-Sales Other' => '305-Sales Other',
+	'306-Internet Sales' => '306-Internet Sales',
+	'307-Service-Hardware Labor' => '307-Service-Hardware Labor',
+	'308-Sales-Books' => '308-Sales-Books',
 );
 
 $jsLanguageStrings = array(
-	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to delete?',
-	'LBL_DELETE_CONFIRMATION' => 'Deleting this Vendor will remove its related PurchaseOrders. Are you sure you want to delete this Vendor?',
-	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this vendor(s) will remove its related Purchase Orders. Are you sure you want to delete the selected Records?',
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Tem certeza que deseja excluir?',
+	'LBL_DELETE_CONFIRMATION' => 'A exclusão deste vendedor removerá seus pedidos de compra relacionados. Tem certeza de que deseja excluir este fornecedor?',
+	'LBL_MASS_DELETE_CONFIRMATION' => 'A exclusão deste(s) fornecedor(es) removerá seus pedidos de compra relacionados. Tem certeza de que deseja excluir os registros selecionados?',
 );
